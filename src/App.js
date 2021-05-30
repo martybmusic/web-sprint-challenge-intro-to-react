fdcebf1382ac81b74257ebb8c3eeb6a13b32f2f2
Martin Brueggemann <marty.brueggemann@Martins-MBP.mediacom.info>
2021-05-30 09:50:39 -0500

    Building App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import styled from 'styled-components';
+import Character from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [ swData, setSwData ] = useState[]
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(result => {
+      console.log(result)
+      setSwData(result.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
 
   return (
     <div className="App">
