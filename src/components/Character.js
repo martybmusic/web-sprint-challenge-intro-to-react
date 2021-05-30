23c92382b47a72a039e72f5e3a692be6034cd412
Martin Brueggemann <marty.brueggemann@Martins-MBP.mediacom.info>
2021-05-30 09:42:03 -0500

    Character cont.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const CharacterStyled = styled.div`
+    color: darkslategrey;
+    background-color: ivory;
+`
+const NameStyled = styled.h3`
+    color: lightslategrey;
+    border-style: inset;
+`
+
 const Character = (props) => {
     const { data } = props
     return (
+        <CharacterStyled>
         <div className = 'name'>
-            {data.name}
+            <NameStyled>{data.name}</NameStyled>
         </div>
         <div className = 'info'>
             <p>Birth Year: {data.birth_year}</p>
-            <
+            <p>Homeworld: {data.homeworld}</p>
+            <p>Height: {data.height}</p>
+            <p>Hair Color: {data.hair_color}</p>
+            <p>Eye Color: {data.eye_color}</p>
+            <p>Films: {data.films}</p>
         </div>
+        </CharacterStyled>
     )
 }
+
+export default Character
