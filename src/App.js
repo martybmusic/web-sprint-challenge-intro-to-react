e7c5597a3805398a5bba161f66845107970e7e52
Martin Brueggemann <marty.brueggemann@Martins-MBP.mediacom.info>
2021-05-30 11:22:52 -0500

    Working on Expanding Button

--- src/App.js
+++ src/App.js
@@ -6,23 +6,39 @@ import Character from './components/Character';
 
 const App = () => {
   const [ swData, setSwData ] = useState([])
+  const [charId, setCharId] = useState(null)
+  const moreDetails = name => {
+    setCharId(name)
+  }
+  const closeDetails = () => {
+    setCharId(null)
+  }
   useEffect(() => {
     axios.get('https://swapi.dev/api/people/')
-    .then(result => {
-      console.log(result)
-      setSwData(result.data)
-    })
+    .then(({data}) => setSwData(data))
     .catch(error => {
       console.log(error)
     })
   }, [])
 
+  const Characters = props => (
+    <div className='character'>
+      {props.info.name}
+      <button onClick={() => moreDetails(props.info.name)}>
+      more
+      </button>
+    </div>
+  )
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="characterNames">
       {swData.map(item => {
-        return <Character data={item} />
+        return <Characters key={item.name} info={item}/>
       })}
+      {charId && <Character name ={charId} close={closeDetails} />}
+    </div>
     </div>
   );
 }
