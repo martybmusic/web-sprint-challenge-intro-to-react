82c1afcb0e12e5ea291742cdf50429b99c701558
Martin Brueggemann <marty.brueggemann@Martins-MBP.mediacom.info>
2021-05-30 11:40:26 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -6,39 +6,23 @@ import Character from './components/Character';
 
 const App = () => {
   const [ swData, setSwData ] = useState([])
-  const [charId, setCharId] = useState(null)
-  const moreDetails = name => {
-    setCharId(name)
-  }
-  const closeDetails = () => {
-    setCharId(null)
-  }
   useEffect(() => {
     axios.get('https://swapi.dev/api/people/')
-    .then(({data}) => setSwData(data))
+    .then(result => {
+      console.log(result)
+      setSwData(result.data)
+    })
     .catch(error => {
       console.log(error)
     })
   }, [])
 
-  const Characters = props => (
-    <div className='character'>
-      {props.info.name}
-      <button onClick={() => moreDetails(props.info.name)}>
-      more
-      </button>
-    </div>
-  )
-
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <div className="characterNames">
       {swData.map(item => {
-        return <Characters key={item.name} info={item}/>
+        return <Character data={item} />
       })}
-      {charId && <Character name ={charId} close={closeDetails} />}
-    </div>
     </div>
   );
 }
