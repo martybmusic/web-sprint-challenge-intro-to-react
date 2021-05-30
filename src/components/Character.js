83f39a9a0911d4fba758f0c976ed7d059d0e1b1c
Martin Brueggemann <marty.brueggemann@Martins-MBP.mediacom.info>
2021-05-30 09:28:50 -0500

    Building Character Component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
-// Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const Character = (props) => {
+    const { data } = props
+    return (
+        <div className = 'name'>
+            {data.name}
+        </div>
+        <div className = 'info'>
+            <p>Birth Year: {data.birth_year}</p>
+            <
+        </div>
+    )
+}
